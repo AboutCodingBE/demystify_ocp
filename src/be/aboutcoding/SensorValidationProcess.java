a715f6cad41669d9cbfd18445ad95ff1e1031c25
nicholas Ocket <facet.it.be@gmail.com>
2023-03-30 10:27:34 +0200

    added the step for getting or creating the sensors

--- src/be/aboutcoding/SensorValidationProcess.java
+++ src/be/aboutcoding/SensorValidationProcess.java
@@ -10,29 +10,11 @@ import java.util.Random;
 public class SensorValidationProcess {
 
     private static final String VALID_FIRMWARE_VERSION = "59.1.12Rev4";
+    private SensorRepository repository = new SensorRepository();
 
     public void start(int... ids) {
-        var temperatureSensors = new ArrayList<Sensor>(ids.length);
-        for (int id : ids) {
-            Random randomGenerator = new Random();
-            var make = "ProSense";
-            var model = "T1000";
-            var randomMajor = randomGenerator.nextInt(60) + 1;
-            var randomMinor = randomGenerator.nextInt(20) + 1;
-            var randomPatdch = randomGenerator.nextInt(120) + 1;
-            var randomRevision = randomGenerator.nextInt(10) + 1;
-
-            String version = randomMajor + "." +
-                    randomMinor +
-                    "." +
-                    randomPatdch +
-                    "Rev" +
-                    randomRevision;
-
-            System.out.println("Id: " + id + " has version: " + version);
-
-            temperatureSensors.add(new Sensor(id, version, make, model));
-        }
+        //Step 1: get sensors for the following ids
+        var temperatureSensors = repository.getSensorsWithIdIn(ids);
 
         var invalidSensors = new ArrayList<Sensor>();
         for (var sensor : temperatureSensors) {
